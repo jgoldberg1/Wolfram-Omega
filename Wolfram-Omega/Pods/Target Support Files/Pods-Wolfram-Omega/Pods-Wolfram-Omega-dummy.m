a4d29ee099e280256406cdc1ba556f4054e1a726
Ayesha Nabiha <anabiha305@gmail.com>
2018-07-16 15:19:14 -0700

    Create view

---
+++ Wolfram-Omega/Pods/Target Support Files/Pods-Wolfram-Omega/Pods-Wolfram-Omega-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Wolfram_Omega : NSObject
+@end
+@implementation PodsDummy_Pods_Wolfram_Omega
+@end
--- Wolfram-Omega/Wolfram-Omega/ViewController.swift
+++ Wolfram-Omega/Wolfram-Omega/ViewController.swift
@@ -9,17 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var continueButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        continueButton.layer.cornerRadius = 15
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        
     }
-    
+
+
 }
 
