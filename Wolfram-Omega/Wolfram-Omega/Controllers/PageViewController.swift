2577df0dc99c86df5ce05f73964deb807049ad3e
Ayesha Nabiha <anabiha305@gmail.com>
2018-07-18 14:15:47 -0700

    Created a PageViewController
    Created the tabs layout, with one of the views hosting a video.

--- Wolfram-Omega/Wolfram-Omega/Controllers/PageViewController.swift
+++ Wolfram-Omega/Wolfram-Omega/Controllers/PageViewController.swift
@@ -8,10 +8,100 @@
 
 import UIKit
 
-class PageViewController: UIPageViewController {
-   
+class PageViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+
+    //stores the page view controllers
+    lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newVC(viewController: "landingPage"),self.newVC(viewController: "helpView")]
+    }()
+    
+    var pageControl = UIPageControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.dataSource = self
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
+        self.delegate = self
+        configurePageControl()
+
+        // Do any additional setup after loading the view.
     }
     
+    func configurePageControl() {
+        //places the page control at the bottom of the screen
+        pageControl = UIPageControl(frame: CGRect(x:0,y:UIScreen.main.bounds.maxY-50, width: UIScreen.main.bounds.width, height: 50))
+        pageControl.numberOfPages = orderedViewControllers.count
+        pageControl.currentPage = 0
+        pageControl.tintColor = UIColor.gray
+        pageControl.pageIndicatorTintColor = UIColor.gray
+        pageControl.currentPageIndicatorTintColor = UIColor.white
+        self.view.addSubview(pageControl)
+    }
+    
+    //returns a view controller from Tabs storyboard
+    func newVC(viewController: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier:viewController)
+    }
+    
+    //returns the view controller before the current one (swiping left)
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        //check if we can get the current view controller index
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else { return nil }
+        
+        let previousIndex = viewControllerIndex-1
+        //check if previousIndex is greater than or equal to 0 to check to make sure that the indicies are within the bounds of the orderedViewControllers array
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        //check to see if there are any more view controllers
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+        
+    }
+    
+    //returns the view controller after the current one (swiping right)
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+       //check if we can get the current view controller index
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else { return nil }
+        
+        let nextIndex = viewControllerIndex+1
+        
+        guard orderedViewControllers.count != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        let pageContentViewController = pageViewController.viewControllers![0]
+        self.pageControl.currentPage = orderedViewControllers.index(of: pageContentViewController)!
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
