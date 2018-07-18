5c0a9f138171f503e998372397da7c183b15fefa
Ayesha Nabiha <anabiha305@gmail.com>
2018-07-18 10:06:35 -0700

    Changes were made

---
+++ Wolfram-Omega/Wolfram-Omega/Controllers/PageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PageViewController.swift
+//  Wolfram-Omega
+//
+//  Created by Ayesha Nabiha on 7/18/18.
+//  Copyright © 2018 Ayesha Nabiha. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
